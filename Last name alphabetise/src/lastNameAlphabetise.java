16c347d570235dfe188762bd40b3fed4bcfa7418
Felicianna.DeSantos <Felicianna.DeSantos@MU-C1-06.mullen.pvt.k12.co.us>
2026-01-26 14:57:36 -0700

    first commit

---
+++ Last name alphabetise/src/lastNameAlphabetise.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+
+public static void lastNameAlphabetise() throws IOException;
+	{
+		
+ Scanner studentList = new Scanner(new File(names.txt));
+	}
